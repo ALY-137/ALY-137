5fd66d9d2e0c79825a8955281142a55d2d06149f
Aly <cherrypassarela@gmail.com>
2022-06-18 21:12:56 -0300

    up

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function verifica(){
-    var verificacao = document.getElementById('email');
-    verificacao = verificacao.textContentgit;
-    console.log(verificacao);
+    var verificacao = document.getElementById('verifiedEmail');
+    var verificacao1 = verificacao.textContent;
+    console.log(verificacao1);
     
-    if(verificacao=="true"){
+    if(verificacao1=="true"){
         var recebe = document.getElementById('Escolha');
         recebe.style.display = "block";
 
