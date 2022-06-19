4fd39b5335cb8609b96ae8c17eea0bd20fe3683a
Aly <cherrypassarela@gmail.com>
2022-06-18 22:02:43 -0300

    up

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ function verifica(){
     console.log(verificacao1);
     
     if(verificacao1=="true"){
+        window.location.href ='https://aly-137.com';  
+
         var recebe = document.getElementById('Escolha');
         recebe.style.display = "block";
 
         var recebe = document.getElementById('Matrix');
         recebe.style.display = "block";
+    }else{
+        window.location.href ='https://aly-137.com/login.html'; 
     }
 }
 
