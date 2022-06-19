d790832cf0ab24a03d035c1a78e091219c4ce785
Aly <cherrypassarela@gmail.com>
2022-06-18 22:35:40 -0300

    up

--- matrix.js
+++ matrix.js
@@ -658,3 +658,22 @@ function start3() {
 function abreLink(){
 	window.location.href ='https://github.com/ALY-137/';  
 }
+
+function verifica(){
+    var verificacao = document.getElementById('verifiedEmail');
+    var verificacao1 = verificacao.textContent;
+    console.log(verificacao1);
+    
+    if(verificacao1=="true"){
+        window.location.href ='https://aly-137.com';  
+
+        var recebe = document.getElementById('Escolha');
+        recebe.style.display = "block";
+
+        var recebe = document.getElementById('Matrix');
+        recebe.style.display = "block";
+    }else{
+        window.location.href ='https://aly-137.com/login.html'; 
+    }
+}
+
