7b93fad1cb69513af57308bb58221e0e1cc09162
Aly <cherrypassarela@gmail.com>
2022-07-21 14:05:01 -0300

    up

--- app.js
+++ app.js
@@ -1,56 +1,74 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAgkMZfiWEqa-KdIEcqm5Z566L_8oGzRqY",
-  authDomain: "aly-137-fbf04.firebaseapp.com",
-  projectId: "aly-137-fbf04",
-  storageBucket: "aly-137-fbf04.appspot.com",
-  messagingSenderId: "983100320011",
-  appId: "1:983100320011:web:43c2ab72720981ea709a0e"
+  apiKey: "AIzaSyCFp02SalV1tohrf6QtVBK2ocPFWl4N9LM",
+  authDomain: "teste-aa015.firebaseapp.com",
+  projectId: "teste-aa015",
+  storageBucket: "teste-aa015.appspot.com",
+  messagingSenderId: "99960275074",
+  appId: "1:99960275074:web:e2923f7e34a0c0c18c749b"
 };
 
-
-  firebase.initializeApp(firebaseConfig);
-
-  function verificaLogin(){
-    
-    var verificacao = document.getElementById('verifiedEmail');
-    verificacao = verificacao.textContent;
-
-    var idGoogle = document.getElementById('sub');
-    idGoogle = idGoogle.textContent;
-
-    var nomeGoogle = document.getElementById('given_name');
-    nomeGoogle = nomeGoogle.textContent;
-
-    var emailGoogle = document.getElementById('email');
-    emailGoogle = emailGoogle.textContent;
-
-    var picGoogle = document.getElementById('picture');
-    picGoogle = picGoogle.src;
-
-    var nomeCompletoGoogle = document.getElementById('fullName');
-    nomeCompletoGoogle = nomeCompletoGoogle.textContent;
-    
-    
-    if(verificacao=="true"){ 
+firebase.initializeApp(firebaseConfig);
 
 
-        let data = new Date();
-      
-        const user = firebase.firestore().collection("login");
+function verificaLogin(){
+   
+   var verificacao = document.getElementById('verifiedEmail');
+   verificacao = verificacao.textContent;
 
-        const novoLogin = {
-        idGoogle: idGoogle,    
-        nomeGoogle: nomeGoogle,
-        nomeCompletoGoogle: nomeCompletoGoogle,
-        emailGoogle: emailGoogle,
-        picGoogle: picGoogle,
-        data: data,
-        };
+   var idGoogle = document.getElementById('sub');
+   idGoogle = idGoogle.textContent;
 
-        user.add(novoLogin);
+   var nomeGoogle = document.getElementById('given_name');
+   nomeGoogle = nomeGoogle.textContent;
 
-        console.log("LOGIN EFETUADO!");
-    }
+   var emailGoogle = document.getElementById('email');
+   emailGoogle = emailGoogle.textContent;
 
+   var picGoogle = document.getElementById('picture');
+   picGoogle = picGoogle.src;
+
+   var nomeCompletoGoogle = document.getElementById('fullName');
+   nomeCompletoGoogle = nomeCompletoGoogle.textContent;
+   
+   
+   if(verificacao=="true"){ 
+
+       let bd = firebase.firestore();
+       let data = new Date();
+       
+
+       bd.collection("usuaries").doc('$idGoogle').get().then(function(doc){
+
+       if(doc.exists){
+
+
+           console.log("USUÁRIO EXISTE");
+       
+
+   }else{
+       const user = firebase.firestore().collection("login");
+
+       const novoLogin = {
+       idGoogle: idGoogle,    
+       nomeGoogle: nomeGoogle,
+       nomeCompletoGoogle: nomeCompletoGoogle,
+       emailGoogle: emailGoogle,
+       picGoogle: picGoogle,
+       data: data,
+       };
+
+       user.add(novoLogin);
+
+       console.log("LOGIN EFETUADO!");
+   }
+
+});
+
+
+
+       
+   
+
+              
+   }
 };
-
