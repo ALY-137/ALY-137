4a7e7a65d77a5c02a8ecc704f591226af13bc13c
Savanna  Passarela <57689838+ALY-137@users.noreply.github.com>
2022-08-11 18:32:04 -0300

    up

---
+++ config.js
@@ -0,0 +1,11 @@
+const largura = window.innerWidth;
+const altura = window.innerHeight;
+
+
+var fundo = document.getElementById('fundo');
+fundo.style.height = `${altura}px`;
+fundo.style.width = `${largura}px`;
+
+
+
+
